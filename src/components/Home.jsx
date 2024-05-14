a508994f378a243720c429915ad19a7610aa1ae6
Vivian <ndidiranjo@gmail.com>
2024-05-14 11:41:36 +0100

    contact

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Contact from './Contact'
 
 const Home = () => {
   return (
-      <div>
+    <div className="flex  h-screen ">
+       <div className="">
         <Navbar/>
-    <div className='flex bg-sec w-[90%] h-[90vh] justify-end'>
+        <div className='flex'>
+        <Contact/>
+    <div className='flex bg-sec w-[85%] '>
         <p>Hi, I'm Amaranjo Ndidi Vivian, transitioning into full-stack development. With a BSc in Geography and Environmental Management and also certifications in Human Resources, International Relationship & Diplomacy and Project Management, I bring a diverse skill set.
 
 Currently in intense full-stack training, specializing in HTML, CSS, JavaScript, Node.js, Express, and MongoDB.
@@ -15,6 +19,8 @@ Explore my coding journey on GitHub. Passionate about effective communication, p
 Excited to make an impact in tech with my background, dedication, and continuous learning. Let's connect on GitHub for more insights and collaboration.
 
 </p>
+</div>
+        </div>
       </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='h-[10vh] bg-pry text-white px-5 flex justify-between font-playfair '>
+    <div className='h-[13vh] relative bg-pry text-white px-5 flex py-5 justify-between font-playfair '>
       <div className='flex'>
         <p>Web Developer</p>
       </div>
-        <div className='flex justify-around font-playfair'>
+        <div className='flex font-playfair justify-between'>
             <p>About</p>
             <p>Education</p>
             <p>Experience</p>
