547d790988a0a9f7ef72ed266303053223eb758d
Vivian <ndidiranjo@gmail.com>
2024-05-15 18:14:05 +0100

    portfolio

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -154,7 +154,7 @@ const LandingPage = () => {
             className="flex w-full justify-center items-center relative"
           >
             <div className="w-[85%] mb-14 flex flex-col text-2xl font-extralight   gap-3 justify-center items-center text-white">
-              <p className="flex mb-5 ">About Me</p>
+              <p className="flex mb-5  font-bold  ">About Me</p>
               <p>
                 Hello, I'm Amaranjo Ndidi Vivian, a passionate Full Stack
                 JavaScript Web Developer based in Abuja, Nigeria. With a
@@ -209,19 +209,20 @@ const LandingPage = () => {
             className="flex w-fulljustify-center items-center relative"
           >
             <div className="w-[85%] mb-14 flex flex-col ml-[7%] text-2xl font-extralight  gap-3 justify-center   text-white">
-              <div className=" flex flex-col pb-3">
-                <p className="item-center justify-center">Education</p>
-                <p>Technical Training</p>
-                <p>Gomycode Abuja</p>
+                <p className="item-center font-bold flex justify-center">Education</p>
+              <div className=" flex gap-2 flex-col pb-3">
+                <p className="item-center font-bold flex justify-center">Technical Training</p>
+                <p className="mb-5  font-bold "  >Gomycode Abuja</p>
+                <div className="flex font-bold  justify-between">
                 <p>Full-Stack JavaScript Development Program</p>
                 <p>2023 - 2024</p>
-                <p>
+                </div>
+                <li>
                   Completed an intensive full-stack JavaScript development
                   program at Gomycode Abuja. Acquired hands-on experience and
                   expertise in modern web development technologies and
                   frameworks, including:
-                </p>
-                <ul>
+                </li>
                   <li>
                     HTML/CSS: Developed skills in creating responsive and
                     visually appealing user interfaces.
@@ -248,27 +249,32 @@ const LandingPage = () => {
                     React.js: Learned React.js library for building efficient
                     and scalable frontend applications.
                   </li>
-                </ul>
               </div>
               <div>
-                <p>Professional Training</p>
-                <p>Clemads Global Consulting Ltd</p>
-                <li>Human Resource Management</li>
+              <p className="item-center mb-5  font-bold  flex justify-center">Professional Training</p>
+                <p className="mb-5 font-bold  ">Clemads Global Consulting Ltd</p>
+                <div className="flex font-bold justify-between mb-5">  
+                <li className="">Human Resource Management</li>
                 <p>2021</p>
+                </div>
                 <p>
                   Acquired comprehensive knowledge and skills in managing human
                   resources effectively, including recruitment, training,
                   performance evaluation, and employee relations.
                 </p>
+                <div className="flex font-bold justify-between mb-5">
                 <li>International Relations & Diplomacy</li>
                 <p>2021</p>
+                </div>
                 <p>
                   Explored the dynamics of international relations and
                   diplomacy, understanding the complexities of global politics,
                   economic interactions, and diplomatic negotiations.
                 </p>
+                <div className="flex font-bold justify-between mb-5">
                 <li>Project Management</li>
                 <p>2021</p>
+                </div>
                 <p>
                   Learned essential project management methodologies and
                   techniques to initiate, plan, execute, monitor, and close
@@ -277,12 +283,17 @@ const LandingPage = () => {
                 </p>
               </div>
               <div>
-                <p>Imo State University</p>
+                <div className="item-center font-bold flex justify-between">
+                <p className="mt-7 mb-3">Imo State University</p>
+                <p >BSc.</p>
+                </div>
+                <div className="flex font-bold justify-between my-5">
                 <p>
                   Bachelor of Science (BSc) in Geography & Environmental
                   Management
                 </p>
                 <p>2014 - 2018</p>
+                </div>
                 <p>Studied and specialized in Geography and Environmental Management, gaining insights into the complex relationship between human societies and the natural environment. Developed analytical skills to assess environmental issues and propose sustainable solutions.</p>
               </div>
             </div>
