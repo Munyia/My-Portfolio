6b86224c89677b001d4cd9be1ee1d4b818cee42a
Vivian <ndidiranjo@gmail.com>
2024-05-22 10:32:40 +0100

    fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,115 +4,14 @@ import Contact from "./Contact";
 import { bg3 } from "../assets/res";
 
 const About = () => {
-  const homeRef = useRef(null);
-  const aboutRef = useRef(null);
-  const educationRef = useRef(null);
-  const experienceRef = useRef(null);
-  const projectsRef = useRef(null);
-  const toolsAndPlatformsRef = useRef(null);
-  const skillsRef = useRef(null);
-
-  const [activeSection, setActiveSection] = useState("");
-
-  const refs = {
-    homeRef,
-    aboutRef,
-    educationRef,
-    experienceRef,
-    projectsRef,
-    toolsAndPlatformsRef,
-    skillsRef,
-  };
-
-  
-
-  const handleScroll = () => {
-    const homeTop = homeRef.current.getBoundingClientRect().top;
-    const aboutTop = aboutRef.current.getBoundingClientRect().top;
-    const educationTop = educationRef.current.getBoundingClientRect().top;
-    const experienceTop = experienceRef.current.getBoundingClientRect().top;
-    const projectsTop = projectsRef.current.getBoundingClientRect().top;
-    const toolsAndPlatformsTop = toolsAndPlatformsRef.current.getBoundingClientRect().top;
-    const skillsTop = skillsRef.current.getBoundingClientRect().top;
-    
-    const offset = window.innerHeight / 2;
-
-    if (skillsTop <= offset) {
-      setActiveSection("Skills");
-    } else if (projectsTop <= offset) {
-      setActiveSection("Projects");
-    } else if (toolsAndPlatformsTop <= offset) {
-      setActiveSection("Tools And Platforms");
-    } else if (experienceTop <= offset) {
-      setActiveSection("Experience");
-    } else if (educationTop <= offset) {
-      setActiveSection("Education");
-    } else if (aboutTop <= offset) {
-      setActiveSection("About");
-    } else if (homeTop <= offset) {
-      setActiveSection("Home");
-    } else {
-      setActiveSection("");
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  useEffect(() => {
-    const options = {
-      root: null,
-      rootMargin: "0px",
-      threshold: 0.5,
-    };
-
-    const callback = (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          setActiveSection(entry.target.getAttribute("data-section"));
-        }
-      });
-    };
-
-    const observer = new IntersectionObserver(callback, options);
-
-    Object.values(refs).forEach((ref) => {
-      if (ref.current) {
-        observer.observe(ref.current);
-      }
-    });
-
-    return () => {
-      Object.values(refs).forEach((ref) => {
-        if (ref.current) {
-          observer.unobserve(ref.current);
-        }
-      });
-    };
-  }, [refs]);
-
-  const scrollToSection = (ref) => {
-    window.scrollTo({
-      top: ref.current.offsetTop,
-      behavior: "smooth",
-    });
-  };
 
   return (
     <div className="relative flex flex-col pt-[5%] h-screen overflow-hidden scroll-m-0">
       <img className="absolute inset-0 w-full h-full object-cover z-0" src={bg3} alt="" />
       <div className="relative z-10 w-full bg-[rgba(0,0,0,0.5)] h-full">
-        <Navbar
-          scrollToSection={scrollToSection}
-          refs={{ homeRef, aboutRef, educationRef, experienceRef, projectsRef, toolsAndPlatformsRef, skillsRef }}
-          activeSection={activeSection}
-        />
+        <Navbar />
         <div className="flex flex-col ml-[15%]  overflow-y-auto h-full pt-16">
         <section
-            ref={aboutRef}
-            data-section="Home"
             className="flex w-full my-[10%] justify-center items-center relative h-screen"
           >
             <div className="w-[85%] pb-[4%] mb-2 text-2xl font-extralight   h-screen flex flex-col gap-3 justify-center items-center text-white">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = ({ scrollToSection, refs, activeSection }) => {
+const Navbar = ({  }) => {
   return (
     <nav className="fixed top-0 w-full h-[12%] bg-sec1 text-white p-4 z-20">
       <div className='flex font-playfair gap-5  text-white justify-between items-center mx-auto'>
@@ -9,25 +9,25 @@ const Navbar = ({ scrollToSection, refs, activeSection }) => {
         <p className="text-2xl ">Web Developer</p>
         </div>
         <div className="flex w-[50%] justify-between"> 
-        <Link to={'/'} onClick={() => scrollToSection(refs.homeRef)} className={`flex bg-sec text-pry p-2 rounded-md  ${activeSection === "Home" ? "underline" : ""}`}>
+        <Link to={'/'}   className={`flex bg-sec text-pry p-2 rounded-md  `}>
           Home
         </Link>
-        <Link to={'/about'} onClick={() => scrollToSection(refs.aboutRef)} className={`flex bg-sec text-pry p-2 rounded-md  ${activeSection === "About" ? "underline" : ""}`}>
+        <Link to={'/about'} className={`flex bg-sec text-pry p-2 rounded-md  `}>
           About
         </Link>
-        <Link to={'/education'} onClick={() => scrollToSection(refs.educationRef)} className={`flex bg-sec text-pry p-2 rounded-md   ${activeSection === "Education" ? "underline" : ""}`}>
+        <Link to={'/education'} className={`flex bg-sec text-pry p-2 rounded-md  `}>
           Education
         </Link>
-        <Link to={"/experience"} onClick={() => scrollToSection(refs.experienceRef)} className={`flex bg-sec text-pry p-2 rounded-md  ${activeSection === "Experience" ? "underline" : ""}`}>
+        <Link to={"/experience"} className={`flex bg-sec text-pry p-2 rounded-md  `}>
           Experience
         </Link>
-        <Link to={'/projects'} onClick={() => scrollToSection(refs.projectsRef)} className={`flex bg-sec text-pry p-2 rounded-md   ${activeSection === "Projects" ? "underline" : ""}`}>
+        <Link to={'/projects'}  className={`flex bg-sec text-pry p-2 rounded-md  `}>
           Projects
         </Link>
-        <Link to={'/tools'} onClick={() => scrollToSection(refs.toolsAndPlatformsRef)} className={`flex bg-sec text-pry p-2 rounded-md ${activeSection === "Tools And Platforms" ? "underline" : ""}`}>
+        <Link to={'/tools'} className={`flex bg-sec text-pry p-2 rounded-md `}>
           Tools and Platforms
         </Link>
-        <Link to={'/skills'} onClick={() => scrollToSection(refs.skillsRef)} className={`flex bg-sec text-pry p-2 rounded-md   ${activeSection === "Skills" ? "underline" : ""}`}>
+        <Link to={'/skills'} className={`flex bg-sec text-pry p-2 rounded-md  `}>
           Skills
         </Link>
         </div>
